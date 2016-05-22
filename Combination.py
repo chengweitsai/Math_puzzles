b435c805aa95e2bb2a279fca87f1bac5e70f21a1
Xwei <tsai.dennis12@gmail.com>
2016-05-22 11:59:43 +0800

    last commmit

--- Combination.py
+++ Combination.py
@@ -7,5 +7,9 @@ def combination(n, r):
 	return combination(n-1, r) + combination(n-1, r-1)
 
 MatrixA = [[0 for x in range(10)] for y in range(40)] 
+"""resualt = combination(40, 10) 
+print " C(40,10) = " +str( resualt)"""
 
-print combination(20, 5)
+
+resualt = combination(990, 33) 
+print " C(990,33) = " +str( resualt)
--- largest_prime.py
+++ largest_prime.py
@@ -1,4 +1,37 @@
 """The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?"""
 
-print 600851475143%13
+import math
+
+def factorize(li, N, D):
+	print "N=", N ,", D=", D 
+	
+	if(N==D):
+		li.append(D)
+		return 
+	if ( N%D == 0):
+		li.append(D)
+		factorize(li, N/D, D)
+	else:
+		factorize(li, N, D+1)
+
+
+def check_prime():
+	print ""
+
+li = [];
+factorize(li, 13195, 2)
+print "13195 = ", li;
+
+Num = 600851475143
+limit = int (math.sqrt(Num) )
+print limit
+ui=[]
+MatrixA = [0 for x in range(limit)] 
+for i in range(2, limit):
+	if(Num % i == 0): 
+		MatrixA[i] =1
+		ui.append(i)
+
+print ui
+print "next find the biggest prime in ui, it is the answer"
